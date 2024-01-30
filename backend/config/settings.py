03b5eb34cb1c1766b15eb81364095d1b7668c4dd
Andrey Agarkov <agarkov@peterpartner.net>
2024-01-30 14:49:52 +0300

    fix - debug value type

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(
     ","
