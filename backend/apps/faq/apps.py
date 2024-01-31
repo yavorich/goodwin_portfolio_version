0552d7c6a1042400bcefdc7fa82e9c425350f411
evgeny <evgeny.suraykin@gmail.com>
2024-01-31 14:22:27 +0300

    upd - Добавлено приложение FAQ, содержащее ответы на часто задаваемые вопросы. Добавлен рабочий роут /api/v1/faq/, возвращающий список вопрос-ответ

--- backend/apps/faq/apps.py
+++ backend/apps/faq/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InformationConfig(AppConfig):
+class FaqConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.information"
+    name = "apps.faq"
