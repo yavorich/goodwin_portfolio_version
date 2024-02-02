a2e9a0aa88c49a8d6814083da38e84fa766fce77
evgeny <evgeny.suraykin@gmail.com>
2024-02-02 14:14:32 +0300

    upd - Разрешения после принятия договора оферты и после подтверждения личности/адреса

---
+++ backend/apps/accounts/permissions.py
@@ -0,0 +1,50 @@
+from rest_framework.permissions import IsAuthenticated
+from django.utils.translation import gettext_lazy as _
+
+from apps.accounts.models import (
+    PersonalVerification,
+    AddressVerification,
+    VerificationStatus,
+)
+
+
+class IsAuthenticatedAndAcceptedOfferAgreement(IsAuthenticated):
+    message = {
+        "code": "offer_agreement_not_accepted",
+        "detail": _(
+            "Для доступа к этой странице необходимо принять условия договора оферты"
+        ),
+    }
+
+    def has_permission(self, request, view):
+        return (
+            super().has_permission(request, view)
+            and request.user.agreement_date is not None
+        )
+
+
+class IsAuthenticatedAndVerified(IsAuthenticatedAndAcceptedOfferAgreement):
+    message = {
+        "code": "user_not_verified",
+        "detail": _(
+            "Для доступа к этой странице необходимо подтвердить свою личность и адрес"
+        ),
+    }
+
+    def has_permission(self, request, view):
+        user = request.user
+        personal_verification: PersonalVerification = getattr(
+            user, "personal_verification", None
+        )
+        address_verification: AddressVerification = getattr(
+            user, "address_verification", None
+        )
+
+        if personal_verification is None or address_verification is None:
+            return False
+
+        return (
+            super().has_permission(request, view)
+            and personal_verification.status == VerificationStatus.APPROVED
+            and address_verification.status == VerificationStatus.APPROVED
+        )
