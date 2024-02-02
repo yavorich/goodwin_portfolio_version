76192da711ba35fdf96200a67badbf38d2b8a5df
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-02 03:52:08 +0300

    add - programs & operations basic functional

--- backend/apps/information/urls.py
+++ backend/apps/information/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+
+router = DefaultRouter()
+router.register("programs", views.UserProgramViewSet, basename="user-programs")
 
 
 urlpatterns = [
     path("operations/", views.OperationAPIView.as_view(), name="operation-list"),
+    path(
+        "operations/<int:pk>/confirm/",
+        views.OperationConfirmAPIView.as_view(),
+        name="operation-confirm",
+    ),
     path("programs/", views.ProgramAPIView.as_view(), name="program-list"),
     path("wallet/", views.WalletAPIView.as_view(), name="wallet-detail"),
-]
+] + router.urls
