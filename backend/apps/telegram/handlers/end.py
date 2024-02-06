dcb0fe721de72d9991ec64e6c583e2141e1823c9
Leonid <titov281@yandex.ru>
2024-02-06 16:02:42 +0500

    upd - add template telegram messages

--- backend/apps/telegram/handlers/end.py
+++ backend/apps/telegram/handlers/end.py
@@ -1,6 +1,8 @@
 from aiogram import types
 from django.contrib.auth import get_user_model
 
+from apps.telegram.models import MessageType
+from apps.telegram.sender import asend_template_telegram_message
 
 User = get_user_model()
 
@@ -10,10 +12,10 @@ async def unlink_user_with_telegram(message: types.Message):
     queryset = User.objects.filter(telegram_id=telegram_id)
 
     if not await queryset.aexists():
-        return await message.reply("Уведомления не привязаны")
+        return await message.reply("Уведомления не подключены")
 
     await User.objects.filter(telegram_id=telegram_id).aupdate(
         telegram_id=None,
         telegram=None,
     )
-    await message.reply(f"Уведомления для телеграмма отвязаны!")
+    await asend_template_telegram_message(telegram_id, MessageType.NOTIFY_DISCONNECTED)
