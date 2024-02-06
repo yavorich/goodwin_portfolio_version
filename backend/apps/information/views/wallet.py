e565a9257641acd94b11ff25fa09e3b8ea131ea4
Andrey Agarkov <business.yavorprod@gmail.com>
2024-02-06 15:19:52 +0300

    upd - add accruals, rework program operations

--- backend/apps/information/views/wallet.py
+++ backend/apps/information/views/wallet.py
@@ -1,9 +1,9 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 
-from apps.information.serializers import WalletSerializer
-from apps.information.models import Wallet
+from apps.information.serializers import WalletSerializer, FrozenItemSerializer
+from apps.information.models import Wallet, FrozenItem
 
 
 class WalletAPIView(RetrieveAPIView):
@@ -12,3 +12,11 @@ class WalletAPIView(RetrieveAPIView):
 
     def get_object(self):
         return get_object_or_404(Wallet, user=self.request.user)
+
+
+class FrozenItemAPIView(ListAPIView):
+    serializer_class = FrozenItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return FrozenItem.objects.filter(wallet=self.request.user.wallet, done=False)
