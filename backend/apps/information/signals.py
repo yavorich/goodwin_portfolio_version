facb9b3fefeec5f1596bd5accb1fe957e002dcef
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-06 15:19:52 +0300

    upd - add accruals, rework program operations

--- backend/apps/information/signals.py
+++ backend/apps/information/signals.py
@@ -1,15 +1,8 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from apps.information.models import FrozenItem, Operation
-from apps.information.tasks import defrost_funds
+from apps.information.models import Operation
 from apps.accounts.services import send_email_confirmation
-
-
-@receiver(post_save, sender=FrozenItem)
-def create_defrost_funds_task(sender, instance: FrozenItem, created, **kwargs):
-    if created:
-        defrost_funds.apply_async(eta=instance.until, args=[instance.pk])
 
 
 @receiver(post_save, sender=Operation)
