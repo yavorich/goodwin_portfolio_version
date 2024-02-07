6b3c74876f9bcc07b8da45ef5141b45ddb1d16e5
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-07 13:29:59 +0300

    fix - program accrual logic

--- backend/apps/information/tasks.py
+++ backend/apps/information/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
 from django.db import transaction
-from django.db.models import Q
 from django.utils.timezone import now, timedelta
 
 from apps.information.models import (
@@ -9,6 +8,7 @@ from apps.information.models import (
     Operation,
     UserProgram,
     UserProgramReplenishment,
+    ProgramResult,
 )
 
 
@@ -43,34 +43,24 @@ def apply_program_start():
 @shared_task
 def make_daily_programs_accruals():
     with transaction.atomic():
-        programs = UserProgram.objects.filter(
-            program__withdrawal_type=Program.WithdrawalType.DAILY
-        )
-        for program in programs:
-            make_program_accruals(program)
-
-
-@shared_task
-def make_finish_programs_accruals():
-    with transaction.atomic():
-        programs = UserProgram.objects.filter(
-            program__withdrawal_type=Program.WithdrawalType.AFTER_FINISH,
-            end_date=now().date(),
-        )
+        programs = Program.objects.filter(accrual_type=Program.AccrualType.DAILY)
         for program in programs:
             make_program_accruals(program)
 
 
 def make_program_accruals(program):
-    latest_result = program.results.latest()
-    if not latest_result or latest_result.created_at < now() - timedelta(days=1):
-        latest_result = program.results.create()
-    user_programs = program.users.filter(Q(status=UserProgram.Status.RUNNING))
+    result = program.results.latest() or ProgramResult.objects.latest(
+        program__isnull=True
+    )
+    if not result or result.created_at < now() - timedelta(days=1):
+        result = ProgramResult.objects.create()
+
+    user_programs = program.users.filter(status=UserProgram.Status.RUNNING)
     for user_program in user_programs:
-        amount = user_program.funds * latest_result.result / 100
+        amount = user_program.funds * result.result / 100
         Operation.objects.create(
             type=Operation.Type.PROGRAM_ACCRUAL,
             wallet=user_program.wallet,
-            amount_free=amount,
+            amount=amount,
             confirmed=True,
         )
