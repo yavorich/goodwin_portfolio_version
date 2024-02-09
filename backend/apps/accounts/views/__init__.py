40a293c5d2d1408ad58541bb5ffa4fce073503a9
Leonid <titov281@yandex.ru>
2024-02-09 18:41:50 +0500

    upd - register and register confirmation

--- backend/apps/accounts/views/__init__.py
+++ backend/apps/accounts/views/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa: F401
 
-from .register import RegisterAPIView
+from .register import RegisterAPIView, RegisterConfirmView
 from .login import LoginAPIView, LoginConfirmView, TokenRefreshAPIView
 from .recover_password import RecoverPasswordAPIView, ResetPasswordAPIView
 from .confirm_email import EmailConfirmAPIView
