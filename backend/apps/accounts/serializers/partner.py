108c54f824131e019275089f930d29a913cdfb12
evgeny <evgeny.suraykin@gmail.com>
2024-02-09 13:49:49 +0300

    upd - Возврат профиля партнёра и региона привязки при получении профиля пользователя

---
+++ backend/apps/accounts/serializers/partner.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+
+from apps.accounts.models import Region
+from apps.accounts.models.user import Partner
+
+
+class RegionSerializer(ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ["id", "name"]
+
+
+class PartnerSerializer(ModelSerializer):
+    region = RegionSerializer()
+
+    class Meta:
+        model = Partner
+        fields = ["id", "partner_id", "region", "partner_fee"]
+
+
+class PartnerRetrieveSerializer(PartnerSerializer):
+    class Meta(PartnerSerializer.Meta):
+        fields = ["partner_id", "region"]
