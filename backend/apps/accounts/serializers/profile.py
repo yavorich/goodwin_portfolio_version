e1acee9e7b8d6d1923f14e11b587876e7339a517
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-12 14:18:38 +0300

    upd - user can remove avatar

--- backend/apps/accounts/serializers/profile.py
+++ backend/apps/accounts/serializers/profile.py
@@ -48,17 +48,16 @@ class ProfileUpdateSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = [
+        required_fields = [
             "full_name",
             "email",
-            "avatar",
             "telegram",
             # "settings",
         ]
-        extra_kwargs = {f: {"required": True} for f in fields}
+        fields = required_fields + ["avatar"]
 
     def validate(self, attrs):
-        for f in self.Meta.fields:
+        for f in self.Meta.required_fields:
             if f not in attrs:
                 raise ValidationError(f"{f} is required")
         if (
@@ -82,7 +81,8 @@ class ProfileUpdateSerializer(ModelSerializer):
 
         instance.email = validated_data["email"]
         instance.telegram = validated_data["telegram"]
-        instance.avatar = validated_data["avatar"]
+        if "avatar" in validated_data:
+            instance.avatar = validated_data["avatar"]
 
         # for attr in validated_data["settings"].keys():
         #     setattr(instance.settings, attr, validated_data["settings"][attr])
