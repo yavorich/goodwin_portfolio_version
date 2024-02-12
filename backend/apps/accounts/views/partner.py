2b3841358d008e441563d5a0bf43c0a06cc5d73c
evgeny <evgeny.suraykin@gmail.com>
2024-02-12 13:40:42 +0300

    upd - Добавлена первая версия представления для получения базовой статистики в профиле партнёра

--- backend/apps/accounts/views/partner.py
+++ backend/apps/accounts/views/partner.py
@@ -1,16 +1,29 @@
 from rest_framework.generics import RetrieveAPIView
 
-from apps.accounts.models.user import Partner
+from apps.accounts.models.user import Partner, User
+from apps.accounts.permissions import IsPartner
+from apps.accounts.serializers.partner import PartnerSerializer
+from apps.information.models import Wallet
+
 
 # from apps.accounts.serializers.partner import PartnerGeneralStatisticsSerializer
 
 
-# class PartnerGeneralStatisticsRetrieveView(RetrieveAPIView):
-#     serializer_class = PartnerGeneralStatisticsSerializer
-#
-#     def get_object(self):
-#         user = self.request.user
-#         return user.partner_profile
-#
-#     def retrieve(self, request, *args, **kwargs):
-#         partner_profile = self.get_object()
+class PartnerGeneralStatisticsRetrieveView(RetrieveAPIView):
+    permission_classes = [IsPartner]
+    serializer_class = PartnerSerializer
+
+    def get_object(self):
+        user = self.request.user
+        return user.partner_profile
+
+    def retrieve(self, request, *args, **kwargs):
+        partner_profile = self.get_object()
+
+        investors = partner_profile.users.all()
+
+        for investor in investors:
+            wallet: Wallet = investor.wallet
+            print(wallet.programs)
+
+        return super().retrieve(request, *args, **kwargs)
