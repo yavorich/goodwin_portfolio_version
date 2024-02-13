4a1f529f8a6c119d93095eaeef7f4f4e94279828
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-13 13:21:25 +0300

    upd - change FloatField to DecimalField

--- backend/apps/information/models/frozen.py
+++ backend/apps/information/models/frozen.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.timezone import now, timedelta
 
-from core.utils import blank_and_null
+from core.utils import blank_and_null, decimal_usdt
 
 
 class FrozenItem(models.Model):
@@ -12,7 +12,7 @@ class FrozenItem(models.Model):
     wallet = models.ForeignKey(
         "Wallet", related_name="frozen_items", on_delete=models.CASCADE
     )
-    amount = models.FloatField()
+    amount = models.DecimalField(**decimal_usdt)
     frost_date = models.DateField(auto_now_add=True)
     defrost_date = models.DateField(**blank_and_null)
     status = models.CharField(choices=Status.choices, default=Status.INITIAL)
