1daf61fbcca9e323d19d193a5a0ed4819f5607c7
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-14 17:34:43 +0300

    upd - Add transfer operation + refactor operations serializers

--- backend/apps/information/serializers/operations.py
+++ backend/apps/information/serializers/operations.py
@@ -1,10 +1,12 @@
 from rest_framework.serializers import (
     ModelSerializer,
     CharField,
+    EmailField,
+    BooleanField,
 )
 from rest_framework.exceptions import ValidationError
 
-from apps.information.models import Operation, Wallet, Action
+from apps.information.models import Operation, Action
 
 
 class OperationSerializer(ModelSerializer):
@@ -25,6 +27,7 @@ class OperationSerializer(ModelSerializer):
 
 
 class OperationCreateSerializer(ModelSerializer):
+    confirmed = BooleanField(read_only=True)
 
     class Meta:
         model = Operation
@@ -32,61 +35,150 @@ class OperationCreateSerializer(ModelSerializer):
             "id",
             "type",
             "wallet",
-            "program",
-            "user_program",
-            "replenishment",
-            "frozen_item",
-            "amount",
-            "amount_free",
-            "amount_frozen",
             "confirmed",
         ]
 
-    def _validate_wallet(self, wallet: Wallet, free=0.0, frozen=0.0):
-        if wallet.free < free:
-            raise ValidationError("Insufficient free funds.")
-        if wallet.frozen < frozen:
-            raise ValidationError("Insufficient frozen funds.")
-
-    def validate(self, attrs):
-        wallet: Wallet = attrs["wallet"]
-        program = attrs.get("program")
-        user_program = attrs.get("user_program")
-        replenishment = attrs.get("replenishment")
-        amount = attrs.get("amount")
+    def _validate_wallet(self, attrs):
         free = attrs.get("amount_free")
         frozen = attrs.get("amount_frozen")
-        # option = self.context.get("option")
-        _type: Operation.Type = attrs["type"]
+        if free and attrs["wallet"].free < free:
+            raise ValidationError("Insufficient free funds.")
+        if frozen and attrs["wallet"].frozen < frozen:
+            raise ValidationError("Insufficient frozen funds.")
 
-        types = Operation.Type
 
-        if _type == types.PROGRAM_START:
-            self._validate_wallet(wallet, free=free, frozen=frozen)
-            if free + frozen < program.min_deposit:
-                raise ValidationError(
-                    f"Minimum program deposit = {program.min_deposit}"
-                )
+class ProgramStartSerializer(OperationCreateSerializer):
+    type = CharField(default=Operation.Type.PROGRAM_START)
 
-        if _type == types.PROGRAM_REPLENISHMENT:
-            self._validate_wallet(wallet, free=free, frozen=frozen)
-            if free + frozen < 100:
-                raise ValidationError(f"Minimum program replenishment = {100}")
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + [
+            "program",
+            "amount_free",
+            "amount_frozen",
+        ]
+        extra_kwargs = {f: {"required": True} for f in fields}
 
-        if _type == types.PROGRAM_REPLENISHMENT_CANCEL:
-            if 0 < replenishment.amount - amount < 100:
-                raise ValidationError(
-                    "Minimum replenishment amount after cancellation - 100 USDT"
-                )
-
-        if _type == types.PROGRAM_CLOSURE:
-            min_deposit = user_program.program.min_deposit
-            if 0 < user_program.funds - amount < min_deposit:
-                raise ValidationError(
-                    f"Minimum program deposit after cancellation - {min_deposit} USDT"
-                )
-
-        if _type == types.DEFROST:
-            self._validate_wallet(wallet, frozen=amount)
-
+    def validate(self, attrs):
+        self._validate_wallet(attrs)
+        if attrs["amount_free"] + attrs["amount_frozen"] < attrs["program"].min_deposit:
+            raise ValidationError(
+                f"Minimum program deposit = {attrs['program'].min_deposit}"
+            )
         return attrs
+
+
+class ProgramReplenishmentSerializer(OperationCreateSerializer):
+    type = CharField(default=Operation.Type.PROGRAM_REPLENISHMENT)
+
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + [
+            "user_program",
+            "amount_free",
+            "amount_frozen",
+        ]
+        extra_kwargs = {f: {"required": True} for f in fields}
+
+    def validate(self, attrs):
+        self._validate_wallet(attrs)
+        if attrs["amount_free"] + attrs["amount_frozen"] < 100:
+            raise ValidationError(f"Minimum program replenishment = {100}")
+        return attrs
+
+
+class ProgramReplenishmentCancelSerializer(OperationCreateSerializer):
+    type = CharField(default=Operation.Type.PROGRAM_REPLENISHMENT_CANCEL)
+
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + [
+            "replenishment",
+            "amount",
+        ]
+        extra_kwargs = {f: {"required": True} for f in fields}
+
+    def validate(self, attrs):
+        if 0 < attrs["replenishment"].amount - attrs["amount"] < 100:
+            raise ValidationError(
+                "Minimum replenishment amount after cancellation - 100 USDT"
+            )
+        return attrs
+
+
+class ProgramClosureSerializer(OperationCreateSerializer):
+    type = CharField(default=Operation.Type.PROGRAM_CLOSURE)
+
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + [
+            "user_program",
+            "amount",
+        ]
+        extra_kwargs = {f: {"required": True} for f in fields}
+
+    def validate(self, attrs):
+        min_deposit = attrs["user_program"].program.min_deposit
+        if 0 < attrs["user_program"].funds - attrs["amount"] < min_deposit:
+            raise ValidationError(
+                f"Minimum program deposit after cancellation - {min_deposit} USDT"
+            )
+        return attrs
+
+
+class WalletDefrostSerializer(OperationCreateSerializer):
+    type = CharField(default=Operation.Type.DEFROST)
+
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + [
+            "amount_frozen",
+        ]
+        extra_kwargs = {f: {"required": True} for f in fields}
+
+    def validate(self, attrs):
+        self._validate_wallet(attrs)
+
+
+class WalletTransferSerializer(OperationCreateSerializer):
+    email = EmailField(write_only=True)
+    type = CharField(default=Operation.Type.TRANSFER)
+
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + [
+            "receiver",
+            "email",
+            "amount_free",
+            "amount_frozen",
+        ]
+        extra_kwargs = {f: {"required": True} for f in fields}
+
+    def validate(self, attrs):
+        self._validate_wallet(attrs)
+        if attrs["email"] != attrs["receiver"].user.email:
+            raise ValidationError("Email is incorrect")
+        return attrs
+
+    def create(self, validated_data: dict):
+        validated_data.pop("email", None)
+        return super().create(validated_data)
+
+
+class WalletReplenishmentSerializer(OperationCreateSerializer):
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + []
+
+
+class WalletWithdrawalSerializer(OperationCreateSerializer):
+    class Meta(OperationCreateSerializer.Meta):
+        fields = OperationCreateSerializer.Meta.fields + []
+
+
+program_operations_serializers = {
+    "start": ProgramStartSerializer,
+    "replenish": ProgramReplenishmentSerializer,
+    "cancel": ProgramReplenishmentCancelSerializer,
+    "close": ProgramClosureSerializer,
+}
+
+wallet_operations_serializers = {
+    "replenish": WalletReplenishmentSerializer,
+    "withdraw": WalletWithdrawalSerializer,
+    "transfer": WalletTransferSerializer,
+    "defrost": WalletDefrostSerializer,
+}
