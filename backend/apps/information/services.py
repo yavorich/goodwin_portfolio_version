75047396b972d0fbb0c5465947c0ad3bddc3a432
Andrey Agarkov <agarkov@peterpartner.net>
2024-02-21 17:25:40 +0300

    upd - Код подтверждения для каждой операции отдельно + lifetime

---
+++ backend/apps/information/services.py
@@ -0,0 +1,19 @@
+from django.utils.translation import gettext_lazy as _
+
+from apps.accounts.tasks import send_email_msg
+from apps.information.models import Operation
+
+
+def send_operation_confirm_email(operation: Operation):
+    title = _("GOODWIN - Подтверждение операции")
+    message = (
+        _(f'Здравствуйте!\nВаш код для подтверждения операции "{operation}"')
+        + f": {operation.confirmation_code}"
+    )
+    send_email_msg.delay(
+        operation.wallet.user.email,
+        title,
+        message,
+        from_email=None,
+        html=False,
+    )
