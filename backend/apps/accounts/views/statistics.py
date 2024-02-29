2e9f366af24b78602641879e97c99a1d80ce5700
evgeny <evgeny.suraykin@gmail.com>
2024-02-29 14:29:33 +0300

    upd - Валидация временного промежутка графика инвестиций партнёра, изменена логика добавления пустых значений к графику

--- backend/apps/accounts/views/statistics.py
+++ backend/apps/accounts/views/statistics.py
@@ -4,7 +4,7 @@ from django.db.models import Sum, F, Window
 from rest_framework.generics import ListAPIView, get_object_or_404
 
 from apps.accounts.permissions import IsAuthenticatedAndVerified
-from apps.accounts.serializers.date import DateRangeSerializer
+from apps.accounts.serializers.date_range import DateRangeSerializer
 from apps.accounts.serializers.statistics import TotalProfitStatisticsGraphSerializer
 from apps.information.models import UserProgramAccrual, UserProgram
 
@@ -19,6 +19,7 @@ class TotalProfitStatisticsGraph(ListAPIView):
         user_program = get_object_or_404(
             UserProgram, pk=user_program_id, wallet=user.wallet
         )
+
         serializer = DateRangeSerializer(data=self.request.query_params)
         serializer.is_valid(raise_exception=True)
 
