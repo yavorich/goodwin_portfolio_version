2c40b9f3a1996e5fa204753b43937fe6fb01f260
Andrey Agarkov <agarkov@peterpartner.net>
2024-03-27 15:01:47 +0300

    upd - Допилил операцию пополнения, + микро фиксы

--- backend/apps/information/services/operation_replenishment_confirmation.py
+++ backend/apps/information/services/operation_replenishment_confirmation.py
@@ -5,19 +5,21 @@ from apps.information.models import Operation
 
 def operation_replenishment_confirmation(operation: Operation, amount):
     if operation.amount > amount:
-        message = _("Insufficient amount transferred")
-    elif operation.amount == amount:
+        return _("Insufficient amount transferred")
+
+    if operation.amount == amount:
         message = _(
             "The required amount has been transferred and credited to the account"
         )
-        operation._to_wallet()
     else:
         message = _(
             "More than the required amount has been transferred and credited to "
             "the account"
         )
         operation.amount = amount
-        operation.save()
-        operation._to_wallet()
+
+    operation.done = True
+    operation.save()
+    operation._to_wallet()
 
     return message
