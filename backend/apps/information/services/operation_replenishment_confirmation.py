7a8d62400fd86018710f1943681d8b7b10766194
Andrey Agarkov <business.yavorprod@gmail.com>
2024-04-11 17:02:37 +0300

    upd - mock replenishment confirmation for DEBUG mode

--- backend/apps/information/services/operation_replenishment_confirmation.py
+++ backend/apps/information/services/operation_replenishment_confirmation.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.utils.translation import gettext_lazy as _
 
 from apps.information.models import Operation
+from config.settings import DEBUG
 
 
 def operation_replenishment_confirmation(operation: Operation, amount):
@@ -9,10 +10,10 @@ def operation_replenishment_confirmation(operation: Operation, amount):
     на криптокошелёк goodwin"""
     if operation.done:
         return _("Operation already done")
-    if operation.amount > amount:
+    if (operation.amount > amount) and not DEBUG:
         return _("Insufficient amount transferred")
 
-    if operation.amount == amount:
+    if (operation.amount == amount) or DEBUG:
         message = _(
             "The required amount has been transferred and credited to the account"
         )
