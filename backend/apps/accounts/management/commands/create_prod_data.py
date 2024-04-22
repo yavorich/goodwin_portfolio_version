ecbb4487c2eb49467612215f282d6dbdfa368fec
Leonid <titov281@yandex.ru>
2024-04-22 17:35:55 +0500

    fix - create_prod_data command

--- backend/apps/accounts/management/commands/create_prod_data.py
+++ backend/apps/accounts/management/commands/create_prod_data.py
@@ -35,24 +35,36 @@ class Command(BaseCommand):
         with connect(settings.BASE_DIR / "goodwinprod.sqlite3") as connection:
             cursor = connection.cursor()
 
-            # create_users(cursor)
-            # create_user_settings(cursor)
-            # update_refer(cursor)
-            # update_wallet_free(cursor)
-            # update_wallet_frozen(cursor)
-            # create_programs(cursor)
-            # create_user_programs(cursor)
-            # create_user_program_accruals(cursor)
-            # create_operation_history_withdraw(cursor)
-            # create_operation_history_partner(cursor)
-            # create_operation_history_program_replenishment(cursor)
-            # create_operation_history_replenishment(cursor)
-            # create_operation_history_extra_fee(cursor)
+            functions = [
+                clean_users,
+                create_users,
+                create_user_settings,
+                update_refer,
+                update_wallet_free,
+                update_wallet_frozen,
+                create_programs,
+                create_user_programs,
+                create_user_program_accruals,
+                create_operation_history_withdraw,
+                create_operation_history_partner,
+                create_operation_history_program_replenishment,
+                create_operation_history_replenishment,
+                create_operation_history_extra_fee,
+                create_operation_history_start_close_program,
+                # imitation_working_app,  no work
+            ]
 
-            create_operation_history_start_close_program(cursor)
-            # imitation_working_app(cursor)
+            stages = len(functions)
+            for i, func in enumerate(functions):
+                func(cursor)
+                print(f"[{i+1}/{stages}] {func.__name__}")
 
         print("Success")
+
+
+def clean_users(cursor):
+    cursor.execute("SELECT email FROM user")
+    User.objects.filter(email__in=[email for (email,) in cursor.fetchall()]).delete()
 
 
 def create_users(cursor):
@@ -655,40 +667,37 @@ def create_operation_history_start_close_program(cursor):
                 "JOIN user ON program_deposit_statistic.user_id = user.id "
                 "JOIN program ON program_deposit_statistic.program_id = program.id "
                 f"WHERE email = '{user.email}' AND program.name = '{program.name}'"
+                "ORDER BY program_deposit_statistic.created_at"
             )
 
             result = cursor.fetchall()
-            user_program_end = [
-                (get_datetime_from_iso(created_at).date(), Decimal(round(deposit, 2)))
-                for email, deposit, created_at, program_name in result
-                if deposit < 0
-            ]
+
+            c_ups = user_programs.filter(program_id=program_id)
 
             user_program_start = [
                 (get_datetime_from_iso(created_at).date(), Decimal(round(deposit, 2)))
                 for email, deposit, created_at, program_name in result
                 if deposit > 0
             ]
-
-            c_ups = user_programs.filter(program_id=program_id)
-
             for user_program in c_ups:
                 c_dates = filter(
                     lambda t: t[0]
                     >= user_program.created_at.date() - timezone.timedelta(days=1),
                     user_program_start,
                 )
+
                 c_dates = list(
                     map(
                         lambda x: (
                             abs((x[0] - user_program.created_at.date()).days),
+                            # abs((x[1] - ))
                             x[0],
                             x[1],
                         ),
                         c_dates,
                     )
                 )
-                c_dates.sort()
+                c_dates.sort(key=lambda x: x[0])
 
                 if len(c_dates) == 0:
                     continue
@@ -698,90 +707,137 @@ def create_operation_history_start_close_program(cursor):
                     user_program_start.index((choice[1], choice[2]))
                 )[1]
 
+                funds = Decimal(get_upe_deposit(cursor, user_program))
                 user_program.deposit = deposit
-                user_program.profit -= deposit
-                # user_program.save()
+                user_program.profit = funds - deposit
+                user_program.save()
+
+                up = UserProgram.objects.get(pk=user_program.pk)
+
+                start_date = get_datetime_from_iso(
+                    get_upe_start_date(cursor, user_program)
+                )
+                create_operation_history_start_program(
+                    user_program, deposit, start_date
+                )
+
+            user_program_end = [
+                (get_datetime_from_iso(created_at), Decimal(round(deposit, 2)))
+                for email, deposit, created_at, program_name in result
+                if deposit < 0
+            ]
+            for user_program in c_ups.filter(status=UserProgram.Status.FINISHED):
+                c_dates = filter(
+                    lambda t: t[0].date()
+                    >= user_program.created_at.date() - timezone.timedelta(days=1),
+                    user_program_end,
+                )
+
+                funds = Decimal(get_upe_deposit(cursor, user_program))
+
+                c_dates = list(
+                    map(
+                        lambda x: (
+                            x[0],
+                            x[1],
+                            abs((x[0].date() - user_program.close_date).days),
+                            abs(
+                                -(
+                                    user_program_end[
+                                        user_program_end.index((x[0], x[1]))
+                                    ]
+                                )[1]
+                                - funds
+                            ),
+                        ),
+                        c_dates,
+                    )
+                )
+                c_dates.sort(key=lambda x: (x[2], x[3]))
+
+                if len(c_dates) == 0:
+                    continue
+
+                choice = c_dates[0]
+                date_, deposit = user_program_end.pop(
+                    user_program_end.index((choice[0], choice[1]))
+                )
+
+                user_program.close_date = date_
+                amount = -deposit
+                user_program.save()
+                create_operation_history_close_program(user_program, amount, date_)
 
 
+def create_operation_history_start_program(user_program, deposit, start_date):
+    OperationHistory.objects.update_or_create(
+        wallet=user_program.wallet,
+        type=OperationHistory.Type.TRANSFER_BETWEEN,
+        created_at=start_date,
+        defaults={
+            "description": dict(
+                ru=f"Запуск программы {user_program.name}",
+                en=f"Starting the {user_program.name} program",
+                cn=None,
+            ),
+            "target_name": user_program.wallet.name,
+            "amount": -deposit,
+        },
+    )
+    OperationHistory.objects.update_or_create(
+        wallet=user_program.wallet,
+        type=OperationHistory.Type.TRANSFER_BETWEEN,
+        created_at=start_date,
+        defaults={
+            "description": dict(
+                ru=f"Программа {user_program.name} пополнена",
+                en=f"Program {user_program.name} has been replenished",
+                cn=None,
+            ),
+            "target_name": user_program.name,
+            "amount": deposit,
+        },
+    )
 
 
-# def create_operation_history_start_program():
-#     OperationHistory.objects.create(
-#         wallet=self.wallet,
-#         type=OperationHistory.Type.TRANSFER_BETWEEN,
-#         description=dict(
-#             ru=f"Запуск программы {self.user_program.name}",
-#             en=f"Starting the {self.user_program.name} program",
-#             cn=None,
-#         ),
-#         target_name=self.wallet.name,
-#         amount=-(self.amount_free + self.amount_frozen),
-#     )
-#     OperationHistory.objects.create(
-#         wallet=self.wallet,
-#         type=OperationHistory.Type.TRANSFER_BETWEEN,
-#         description=dict(
-#             ru=f"Программа {self.user_program.name} пополнена",
-#             en=f"Program {self.user_program.name} has been replenished",
-#             cn=None,
-#         ),
-#         target_name=self.user_program.name,
-#         amount=self.amount_free + self.amount_frozen,
-#     )
+def create_operation_history_close_program(user_program, amount, close_date):
+    if user_program.end_date != close_date.date():
+        description = dict(
+            ru=f"Программа {user_program.name} закрыта досрочно",
+            en=f"The {user_program.name} program is closed early",
+            cn=None,
+        )
+    else:
+        description = dict(
+            ru=f"Программа {user_program.name} закрыта",
+            en=f"{user_program.name} program is closed",
+            cn=None,
+        )
 
-
-# def create_operation_history_close_program():
-#     if self.partial:
-#         description = dict(
-#             ru=f"Частичное закрытие программы {self.user_program.name}",
-#             en=f"Partial closure of the {self.user_program.name} program",
-#             cn=None,
-#         )
-#         self.user_program.update_deposit(amount=-self.amount)
-#     else:
-#         if self.early_closure:
-#             description = dict(
-#                 ru=f"Программа {self.user_program.name} закрыта досрочно",
-#                 en=f"The {self.user_program.name} program is closed early",
-#                 cn=None,
-#             )
-#         else:
-#             description = dict(
-#                 ru=f"Программа {self.user_program.name} закрыта",
-#                 en=f"{self.user_program.name} program is closed",
-#                 cn=None,
-#             )
-#         self.user_program.close()
-#         replenishments = self.user_program.replenishments.filter(
-#             status=UserProgramReplenishment.Status.INITIAL
-#         )
-#         for replenishment in replenishments:
-#             Operation.objects.create(
-#                 type=Operation.Type.PROGRAM_REPLENISHMENT_CANCEL,
-#                 wallet=self.wallet,
-#                 replenishment=replenishment,
-#                 amount=replenishment.amount,
-#                 confirmed=True,
-#             )
-#     self.wallet.update_balance(frozen=self.amount)
-#     OperationHistory.objects.create(
-#         wallet=self.wallet,
-#         type=OperationHistory.Type.SYSTEM_MESSAGE,
-#         description=description,
-#         target_name=self.user_program.name,
-#         amount=-self.amount,
-#     )
-#     OperationHistory.objects.create(
-#         wallet=self.wallet,
-#         type=OperationHistory.Type.TRANSFER_FROZEN,
-#         description=dict(
-#             ru=f"Перевод депозита {self.user_program.name}",
-#             en=f"Transfer of deposit {self.user_program.name}",
-#             cn=None,
-#         ),
-#         target_name=self.wallet.name,
-#         amount=self.amount,
-#     )
+    OperationHistory.objects.update_or_create(
+        wallet=user_program.wallet,
+        type=OperationHistory.Type.SYSTEM_MESSAGE,
+        created_at=close_date,
+        defaults={
+            "description": description,
+            "target_name": user_program.name,
+            "amount": -amount,
+        },
+    )
+    OperationHistory.objects.update_or_create(
+        wallet=user_program.wallet,
+        type=OperationHistory.Type.TRANSFER_FROZEN,
+        created_at=close_date,
+        defaults={
+            "description": dict(
+                ru=f"Перевод депозита {user_program.name}",
+                en=f"Transfer of deposit {user_program.name}",
+                cn=None,
+            ),
+            "target_name": user_program.wallet.name,
+            "amount": amount,
+        },
+    )
 
 
 def imitation_working_app(cursor):
@@ -793,31 +849,16 @@ def imitation_working_app(cursor):
         if not user_programs.exists():
             continue
 
-        cursor.execute(
-            "SELECT email, deposit, "
-            "program_deposit_statistic.created_at AS created_at, "
-            "program.name AS program_name "
-            "FROM program_deposit_statistic "
-            "JOIN user ON program_deposit_statistic.user_id = user.id "
-            "JOIN program ON program_deposit_statistic.program_id = program.id "
-            f"WHERE email = '{user.email}'"
-        )
+        for user_program in user_programs:
+            print(
+                user_program.name,
+                user_program.deposit,
+                user_program.funds,
+                get_upe_deposit(cursor, user_program),
+                user_program.status,
+            )
 
-        user_program_extra_operations = {
-            (
-                get_datetime_from_iso(created_at).date() + timezone.timedelta(days=3),
-                User.objects.get(email=email).pk,
-                Program.objects.get(name=program_name).pk,
-            ): Decimal(deposit)
-            for email, deposit, created_at, program_name in cursor.fetchall()
-        }
-
-        cursor.execute(
-            "SELECT email, amount_gc AS amount, finished "
-            "FROM invoice "
-            "JOIN user ON invoice.user_id = user.id "
-            "WHERE invoice.status = '2'"
-        )
+        continue
 
         start_date = user_programs.order_by("created_at").first().created_at.date()
         end_date = user_programs.order_by("-close_date").first().close_date
@@ -829,7 +870,7 @@ def imitation_working_app(cursor):
         user_programs_data = {
             user_program.pk: [
                 user_program,
-                0,
+                user_program.deposit,
                 {
                     accrual.created_at: accrual
                     for accrual in user_program.accruals.iterator()
@@ -871,51 +912,28 @@ def imitation_working_app(cursor):
                 if len(p_cur_ups) == 0:
                     continue
 
-                today_created_ups = list(
-                    filter(lambda up: up.created_at.date() == current_date, p_cur_ups)
-                )
+                for user_program in p_cur_ups:
+                    user_program_data = user_programs_data[user_program.pk]
 
-                if len(today_created_ups) > 0:
-                    print(len(today_created_ups))
+                    if current_date in user_program_data[2].keys():
+                        user_program_data[1] += user_program_data[2][
+                            current_date
+                        ].amount
+                        # print(
+                        #     user_program_data[1], user_program_data[2][current_date][1]
+                        # )
 
-                # if (
-                #     current_date,
-                #     user.pk,
-                #     program_id,
-                # ) in user_program_extra_operations.keys():
-                #     amount = user_program_extra_operations[
-                #         (current_date, user.pk, program_id)
-                #     ]
-                #
-                #     if len(p_cur_ups) > 1:
-                #         pass
-                #         # print(p_cur_ups, amount)
-                #
-                #     else:
-                #         user_program = p_cur_ups[0]
-                #         if current_date == user_program.created_at
-                #
-                #         user_program_data = user_programs_data[user_program.pk]
-                #         user_program_data[1] += amount
+                    if current_date in user_program_data[3].keys():
+                        user_program_data[1] += user_program_data[3][
+                            current_date
+                        ].amount
 
-                # for user_program in p_cur_ups:
-                #     user_program_data = user_programs_data[user_program.pk]
-                #
-                #     if current_date in user_program_data[2].keys():
-                #         user_program_data[1] += user_program_data[2][current_date][0]
-                #         # print(
-                #         #     user_program_data[1], user_program_data[2][current_date][1]
-                #         # )
-                #
-                #     if current_date in user_program_data[3].keys():
-                #         user_program_data[1] += user_program_data[3][current_date]
-
-        # print(
-        #     [
-        #         (upd[1], get_upe_deposit(cursor, upd[0]))
-        #         for upd in user_programs_data.values()
-        #     ]
-        # )
+        print(
+            [
+                (upd[1], get_upe_deposit(cursor, upd[0]), upd[0].funds)
+                for upd in user_programs_data.values()
+            ]
+        )
 
         # accruals = user_program.accruals
         # replenishments = user_program.replenishments
@@ -946,7 +964,8 @@ def get_date_time_from_iso_or_null(_datetime):
 
 def get_upe_deposit(cursor, user_program):
     cursor.execute(
-        "SELECT deposit, user_programs.created_at AS created_at "
+        "SELECT deposit, user_programs.created_at AS created_at, "
+        "user_programs.start start "
         "FROM user_programs "
         "JOIN user ON user_programs.user_id = user.id "
         "JOIN program ON user_programs.program_id = program.id "
@@ -957,7 +976,37 @@ def get_upe_deposit(cursor, user_program):
         map(
             lambda x: x[0],
             filter(
-                lambda X: get_date_time_from_iso_or_null(X[1])
+                lambda x: (
+                    get_datetime_from_iso(x[1])
+                    if x[1]
+                    else get_datetime_from_iso(x[2]) - timezone.timedelta(days=3)
+                )
+                == user_program.created_at,
+                cursor.fetchall(),
+            ),
+        )
+    )
+
+
+def get_upe_start_date(cursor, user_program):
+    cursor.execute(
+        "SELECT user_programs.created_at AS created_at, "
+        "user_programs.start start "
+        "FROM user_programs "
+        "JOIN user ON user_programs.user_id = user.id "
+        "JOIN program ON user_programs.program_id = program.id "
+        f"WHERE email = '{user_program.wallet.user.email}' "
+        f"AND program.name = '{user_program.program.name}'"
+    )
+    return next(
+        map(
+            lambda x: x[1],
+            filter(
+                lambda x: (
+                    get_datetime_from_iso(x[0])
+                    if x[0]
+                    else get_datetime_from_iso(x[1]) - timezone.timedelta(days=3)
+                )
                 == user_program.created_at,
                 cursor.fetchall(),
             ),
