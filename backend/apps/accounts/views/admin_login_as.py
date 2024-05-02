7a1f2c7c116e9446d567ac19ddaf62e2ad50da58
Leonid <titov281@yandex.ru>
2024-05-02 16:15:36 +0500

    fix - login as

--- backend/apps/accounts/views/admin_login_as.py
+++ backend/apps/accounts/views/admin_login_as.py
@@ -1,4 +1,9 @@
-from django.http.response import HttpResponseRedirect
+from django.conf import settings
+from django.http.response import HttpResponseRedirect, Http404
+from django.middleware.csrf import get_token
+from django.shortcuts import get_object_or_404
+
+from django.views import View
 
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
@@ -8,13 +13,26 @@ from apps.accounts.permissions import IsAdmin
 from config.settings import FRONT_URL
 
 
-class LoginAsUserView(APIView):
+class LoginAsUserView(View):
     permission_classes = [IsAdmin]
 
-    def get(self, request, *args, **kwargs):
-        user = User.objects.get(pk=self.kwargs["pk"])
+    def post(self, request, *args, **kwargs):
+        self.check_permissions()
+
+        user = get_object_or_404(User, pk=self.kwargs["pk"])
         response = HttpResponseRedirect(redirect_to=FRONT_URL)
-        refresh = str(RefreshToken.for_user(user))
-        access = str(AccessToken.for_user(user))
-        response["Cookie"] = f"access={access};refresh={refresh}"
+        refresh = RefreshToken.for_user(user)
+        response.set_cookie("refresh_token", str(refresh))
+        response.set_cookie("access_token", str(refresh.access_token))
+        response.set_cookie("csrftoken", str(get_token(request)))
+        response.set_cookie(settings.SESSION_COOKIE_NAME, request.session.session_key)
         return response
+
+    def check_permissions(self):
+        if not all(
+            (
+                permission_class().has_permission(self.request, self)
+                for permission_class in self.permission_classes
+            )
+        ):
+            raise Http404
