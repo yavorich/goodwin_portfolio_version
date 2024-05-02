4fd3ba8094f2783481a90c491f42074110624fc4
Leonid <titov281@yandex.ru>
2024-05-02 16:24:30 +0500

    fix - login as tokens

--- backend/apps/accounts/views/admin_login_as.py
+++ backend/apps/accounts/views/admin_login_as.py
@@ -2,11 +2,8 @@ from django.conf import settings
 from django.http.response import HttpResponseRedirect, Http404
 from django.middleware.csrf import get_token
 from django.shortcuts import get_object_or_404
-
 from django.views import View
-
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.accounts.models import User
 from apps.accounts.permissions import IsAdmin
@@ -22,8 +19,8 @@ class LoginAsUserView(View):
         user = get_object_or_404(User, pk=self.kwargs["pk"])
         response = HttpResponseRedirect(redirect_to=FRONT_URL)
         refresh = RefreshToken.for_user(user)
-        response.set_cookie("refresh_token", str(refresh))
-        response.set_cookie("access_token", str(refresh.access_token))
+        response.set_cookie("refresh", str(refresh))
+        response.set_cookie("access", str(refresh.access_token))
         response.set_cookie("csrftoken", str(get_token(request)))
         response.set_cookie(settings.SESSION_COOKIE_NAME, request.session.session_key)
         return response
