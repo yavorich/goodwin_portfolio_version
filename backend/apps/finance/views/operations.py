0eef4214aee6f5102c6e098a9159436814518884
Andrey Agarkov <business.yavorprod@gmail.com>
2024-05-22 15:26:59 +0300

    upd - add totals to operation history response

--- backend/apps/finance/views/operations.py
+++ backend/apps/finance/views/operations.py
@@ -69,15 +69,18 @@ class OperationAPIView(ListAPIView):
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-
-        if request.query_params.get("page"):
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        total_data = {
+            "total_in": queryset.total_in(),
+            "total_out": queryset.total_out(),
+        }
+        page = self.paginate_queryset(queryset)
+        if request.query_params.get("page") and page is not None:
+            serializer = self.get_serializer(page, many=True)
+            serializer_data = self.get_paginated_response(serializer.data).data
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            serializer_data = {"results": serializer.data}
+        return Response({**total_data, **serializer_data})
 
 
 class OperationConfirmAPIView(UpdateAPIView):
