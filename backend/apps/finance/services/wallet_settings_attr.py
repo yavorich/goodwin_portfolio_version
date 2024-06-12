e6fa5995d516416c9730c97688ad2239f6b8de73
Andrey Agarkov <business.yavorprod@gmail.com>
2024-06-12 14:59:42 +0300

    fix - zero wallet settings attr bug

--- backend/apps/finance/services/wallet_settings_attr.py
+++ backend/apps/finance/services/wallet_settings_attr.py
@@ -5,5 +5,7 @@ def get_wallet_settings_attr(wallet, attr):
     WalletSettings = apps.get_model(app_label="finance", model_name="WalletSettings")
     personal_settings = WalletSettings.objects.get(wallet=wallet)
     base_settings = WalletSettings.objects.get(wallet__isnull=True)
-    value = getattr(personal_settings, attr) or getattr(base_settings, attr)
+    value = getattr(personal_settings, attr)
+    if value is None:
+        value = getattr(base_settings, attr)
     return value
