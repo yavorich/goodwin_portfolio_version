6e5ced6ae6ea238ca3393204af32419cbd7eadf6
Andrey Agarkov <business.yavorprod@gmail.com>
2024-06-25 14:59:09 +0300

    upd - add topup periods list endpoint

--- backend/apps/gdw_site/serializers/__init__.py
+++ backend/apps/gdw_site/serializers/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa: F401
 
 from .programs import SiteProgramSerializer
-from .calculator import CalculatorSerializer
+from .calculator import CalculatorSerializer, TopupPeriod
 from .stats import FundTotalStatsSerializer
