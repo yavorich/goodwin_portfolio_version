0a725a81c90f4ba5271b87d332189ca6e08c8583
Andrey Agarkov <business.yavorprod@gmail.com>
2024-07-03 18:40:39 +0300

    fix - tag field in SiteNewsSerializer

--- backend/apps/gdw_site/serializers/news.py
+++ backend/apps/gdw_site/serializers/news.py
@@ -1,9 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from apps.gdw_site.models import SiteNews
 
 
 class SiteNewsSerializer(ModelSerializer):
+    tag = CharField(source="tag.tag")
+
     class Meta:
         model = SiteNews
         fields = ["id", "image", "title", "text", "tag", "date"]
