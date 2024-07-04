ef088a489395291513cb17e7f3a68ad19f5cdd81
Andrey Agarkov <business.yavorprod@gmail.com>
2024-07-04 13:42:53 +0300

    upd - add pagination for site news list

--- backend/apps/gdw_site/views/news.py
+++ backend/apps/gdw_site/views/news.py
@@ -4,8 +4,11 @@ from rest_framework.permissions import AllowAny
 from apps.gdw_site.models import SiteNews
 from apps.gdw_site.serializers import SiteNewsSerializer
 
+from core.pagination import PageNumberSetPagination
+
 
 class SiteNewsViewSet(ReadOnlyModelViewSet):
     queryset = SiteNews.objects.filter(show_on_site=True)
     permission_classes = [AllowAny]
     serializer_class = SiteNewsSerializer
+    pagination_class = PageNumberSetPagination
