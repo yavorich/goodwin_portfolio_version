819bee53b057f84684214aa31371566432dfbf44
Andrey Agarkov <business.yavorprod@gmail.com>
2024-07-05 21:05:03 +0300

    upd - create celery tasks for syncing news_bot messages

--- backend/apps/gdw_site/signals.py
+++ backend/apps/gdw_site/signals.py
@@ -2,11 +2,11 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from apps.gdw_site.models import SiteNews
-from apps.gdw_site.services.news import find_and_sync_message
+from apps.gdw_site.tasks import find_and_sync_message
 
 
 @receiver(post_save, sender=SiteNews)
 def sync_site_news(sender, instance: SiteNews, created: bool, **kwargs):
     if instance.edited_by_admin and instance.sync_with_tg:
         if not instance.same("sync_with_tg"):
-            find_and_sync_message(instance.message_id)
+            find_and_sync_message.delay(instance.message_id)
