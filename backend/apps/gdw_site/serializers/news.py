81dc244f834628d22d16a044cecaf3de54fa73f2
Andrey Agarkov <business.yavorprod@gmail.com>
2024-07-05 17:00:10 +0300

    fix - image links for site news

--- backend/apps/gdw_site/serializers/news.py
+++ backend/apps/gdw_site/serializers/news.py
@@ -1,10 +1,12 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
 from apps.gdw_site.models import SiteNews
+from core.serializers import HttpsFileField
 
 
 class SiteNewsSerializer(ModelSerializer):
     tag = CharField(source="tag.tag")
+    image = HttpsFileField()
 
     class Meta:
         model = SiteNews
