e33e933b7dd7e16d07bc79ee94739464e9a3b209
Andrey Agarkov <business.yavorprod@gmail.com>
2024-07-09 12:28:27 +0300

    upd - add logo for site social contacts

--- backend/apps/gdw_site/models/social.py
+++ backend/apps/gdw_site/models/social.py
@@ -1,11 +1,17 @@
-from django.db.models import Model
+import os
+from django.db.models import Model, FileField
 
 from core.localized.fields import LocalizedCharField
+
+
+def get_upload_path(instance, filename):
+    return os.path.join("social", instance.service, filename)
 
 
 class SocialContact(Model):
     service = LocalizedCharField("Сервис", max_length=32)
     link = LocalizedCharField("Ссылка", blank=True)
+    logo = FileField(upload_to=get_upload_path, null=True)
 
     def __str__(self) -> str:
         return self.service.ru
--- backend/apps/gdw_site/serializers/social.py
+++ backend/apps/gdw_site/serializers/social.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
+from core.serializers import HttpsFileField
 from apps.gdw_site.models import SocialContact
 
 
 class SocialContactSerializer(ModelSerializer):
     service = CharField()
     link = CharField(allow_null=True)
+    logo = HttpsFileField()
 
     class Meta:
         model = SocialContact
-        fields = ["id", "service", "link"]
+        fields = ["id", "service", "link", "logo"]
