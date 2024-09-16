ae94fadc233332722df879bec5f91f65e5acb8ef
Andrey Agarkov <business.yavorprod@gmail.com>
2024-09-16 12:45:30 +0300

    fix - calculator daily stats does not exist

--- backend/apps/gdw_site/serializers/calculator.py
+++ backend/apps/gdw_site/serializers/calculator.py
@@ -106,7 +106,8 @@ def calculate_program_result(attrs, program: SiteProgram):
     topups = 0
 
     while current_date <= end_date:
-        daily_profit_pct = FundDailyStats.objects.get(date=current_date).percent
+        daily_stats = FundDailyStats.objects.filter(date=current_date).first()
+        daily_profit_pct = daily_stats.percent if daily_stats else 0.0
         today_profit = deposit * daily_profit_pct / 100
         management_fee = deposit * management_fee_pct / 100
         success_fee = max(0, today_profit * success_fee_pct / 100)
